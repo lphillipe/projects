7832ee62b1a170714475a7ae6343a4592bd06b9b
lphillipe <luisphillipe200@gmail.com>
2026-02-05 07:10:14 -0300

    config in cars class brand and car

--- fast_master/car_api/car_api/models/cars.py
+++ fast_master/car_api/car_api/models/cars.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from decimal import Decimal
 from typing import Optional
 
 from sqlalchemy import func 
@@ -16,6 +17,26 @@ class Brand(Base):
     is_active: Mapped[bool] = mapped_column(default=True)
     description: Mapped[Optional[str]] = mapped_column(Text, default=None)
     
+    created_at: Mapped[str] = mapped_column(server_default=func.now())
+    update_at: Mapped[str] = mapped_column(
+        onupdate=func.now(), server_default=func.now(),
+    )
+
+class Car(Base):
+    __tablename__= 'cars'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    model: Mapped[str] = mapped_column(String[100])
+    factory_year: Mapped[int] = mapped_column(Integer)
+    model_year: Mapped[int] = mapped_column(Integer)
+    color: Mapped[str] = mapped_column(String[30])
+    plate: Mapped[str] = mapped_column(String[10], unique=True, index=True)
+
+    price: Mapped[Decimal] = mapped_column(Numeric[10, 2])
+    description: Mapped[Optional[str]] = mapped_column(Text, default=None)
+    is_available: Mapped[bool] = mapped_column(default=True)
+
     created_at: Mapped[str] = mapped_column(server_default=func.now())
     update_at: Mapped[str] = mapped_column(
         onupdate=func.now(), server_default=func.now(),
