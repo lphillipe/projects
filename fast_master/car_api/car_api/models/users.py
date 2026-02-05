bcfee6c29f238d4e19b5fad5cafcab1d8cae6915
lphillipe <luisphillipe200@gmail.com>
2026-02-05 05:58:37 -0300

    update models users

--- fast_master/car_api/car_api/models/users.py
+++ fast_master/car_api/car_api/models/users.py
@@ -1,9 +1,20 @@
+from datetime import datetime
+
+from sqlalchemy import func 
 from sqlalchemy.orm import Mapped, mapped_column
 
 from car_api.models import Base
 
-class User():
+class User(Base):
     __tablename__ = 'users'
 
     id: Mapped[int] = mapped_column(primary_key=True)
     username: Mapped[str] = mapped_column(unique=True)
+    password: Mapped[str]
+    email: Mapped[str] = mapped_column(unique=True)
+    update_at: Mapped[str] = mapped_column(
+        onupdate=func.now(), server_default=func.now(),
+    )
+    created_at: Mapped[str] = mapped_column(
+        server_default=func.now(),
+    )
