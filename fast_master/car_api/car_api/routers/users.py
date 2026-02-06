ae710aee6e14b67c582070fa9a1e74a88b788c93
lphillipe <luisphillipe200@gmail.com>
2026-02-06 07:33:01 -0300

    update routers.users create exceptions error username

--- fast_master/car_api/car_api/routers/users.py
+++ fast_master/car_api/car_api/routers/users.py
@@ -1,6 +1,10 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, HTTPException, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select, exists
 
+from car_api.core.database import get_session
 from car_api.db import USERS
+from car_api.models.users import User
 from car_api.schemas.users import (
     UserSchema,
     UserListPublicSchema,
@@ -14,11 +18,31 @@ router = APIRouter()
     path='/',
     status_code=status.HTTP_201_CREATED,
     response_model=UserPublicSchema,
+    summary='Criar novo usuário',
 )
-async def create_user(user: UserSchema):
-    user_with_id = UserPublicSchema(**user.model_dump(), id=len(USERS) + 1)
-    USERS.append(user_with_id)
-    return user_with_id
+async def create_user(
+    user: UserSchema, 
+    db: AsyncSession = Depends(get_session),
+):
+    username_exists = await db.scalar(
+        select(exists().where(User.username == user.username))
+    )
+    if username_exists:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail='Username já está em uso',
+        )
+    db_user = User(
+        username=user.username,
+        email=user.email,
+        password=user.password,
+    )
+
+    db.add(db_user)
+    await db.commit()
+    await db.refresh(db_user)
+
+    return db_user
 
 
 @router.get(
