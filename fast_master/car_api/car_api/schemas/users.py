dc43d5ad5af00afaeba6dfdeb455185f4cab33ed
lphillipe <luisphillipe200@gmail.com>
2026-02-09 05:49:54 -0300

    update schemas

--- fast_master/car_api/car_api/schemas/users.py
+++ fast_master/car_api/car_api/schemas/users.py
@@ -28,6 +28,19 @@ class UserUpdateSchema(BaseModel):
     password: Optional[str] = None
 
 
+    @field_validator('username')
+    def username_min_length(cls, v):
+        if len(v) <3:
+            raise ValueError('Username deve ter pelo menos 3 caracteres')
+        return v
+    
+    @field_validator('password')
+    def password_min_lenght(cls, v):
+        if len(v) < 6:
+            raise ValueError('Senha deve ter pelo menos 6 caracteres')
+        return v
+
+
 class UserPublicSchema(BaseModel):
     model_config = ConfigDict(from_attributes=True)
     
