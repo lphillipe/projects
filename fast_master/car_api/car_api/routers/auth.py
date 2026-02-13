e38ef7947c5916897fc75d27a112bfef52ee5d12
lphillipe <luisphillipe200@gmail.com>
2026-02-13 16:30:29 -0300

    create router post async token

--- fast_master/car_api/car_api/routers/auth.py
+++ fast_master/car_api/car_api/routers/auth.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from car_api.core.database import get_session
@@ -13,3 +13,25 @@ from car_api.schemas.auth import LoginRequest, Token
 
 
 router = APIRouter()
+
+@router.post(
+    '/token',
+    response_model=Token,
+    status_code=status.HTTP_200_OK,
+    summary='Gerar token de acesso',
+)
+async def token(
+    login_data: LoginRequest,
+    db: AsyncSession = Depends(get_session),
+):
+    user = await authenticate_user(login_data.email, login_data.password, db)
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='Incorrect email or password',
+            headers=('WWW-Authenticate': 'Bearer'),
+        )
+    access_token = create_access_token(data={'sub': str(user.id)})
+
+    return {'access_token': access_token, 'token_type': 'bearer'}
