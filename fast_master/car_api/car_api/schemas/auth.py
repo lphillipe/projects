47b618260397d5f5510a8a448e2a4a5f1d6b1a29
lphillipe <luisphillipe200@gmail.com>
2026-02-13 16:19:11 -0300

    create class Token, LoginRequest in auth schemas

---
+++ fast_master/car_api/car_api/schemas/auth.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel, EmailStr, field_validator
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class LoginRequest(BaseModel):
+    email: EmailStr
+    password: str
+
+    @field_validator('password')
+    def password_min_lenght(cls, v):
+        if len(v) < 6:
+            raise ValueError('Senha deve ter pelo menos 6 caracteres')
+        return v
+
